81657d7c2d6f2991532a573ab62b9ade9f870311
dmitry.brusentsov <dmitry.brusentsov@sharp-dev.net>
2024-01-26 17:34:17 +0800

    first commit

---
+++ BlazorServerMessageWall/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMessageWall.Models
+{
+    public class MessageModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string FirstName { get; set; }
+		[Required]
+		[StringLength(20, MinimumLength = 6)]
+		public string LastName { get; set; }
+	}
+}
